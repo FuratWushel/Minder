cf0aedbbc3b47879accaf1f493da1f37ddc8b838
furat95 <furat95@live.nl>
2018-02-08 22:28:38 +0100

    Add project files.

---
+++ Stardate5/Migrations/201710061325533_useridweg.cs
@@ -0,0 +1,20 @@
+namespace Minder.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class useridweg : DbMigration
+    {
+        public override void Up()
+        {
+            RenameColumn(table: "dbo.Profiles", name: "UserId", newName: "User_Id");
+            RenameIndex(table: "dbo.Profiles", name: "IX_UserId", newName: "IX_User_Id");
+        }
+        
+        public override void Down()
+        {
+            RenameIndex(table: "dbo.Profiles", name: "IX_User_Id", newName: "IX_UserId");
+            RenameColumn(table: "dbo.Profiles", name: "User_Id", newName: "UserId");
+        }
+    }
+}
---
+++ Stardate5/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace Minder.Migrations
+{
+    using Minder.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Minder.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Minder.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            context.Profiles.AddOrUpdate(
+              p => p.Nickname,
+              new Profile { Nickname = "Andrew Peters", City = "Alkmaar", Gender = GenderEnum.Male, Height = 180, Education = EducationEnum.Highschool, Ethnicity = EthnicityEnum.European, Birthdate = new DateTime(1990, 1, 1), GenderInterests = GenderInterestEnum.Women },
+                            new Profile { Nickname = "Andrew Peters", City = "Alkmaar", Gender = GenderEnum.Male, Height = 180, Education = EducationEnum.Highschool, Ethnicity = EthnicityEnum.European, Birthdate = new DateTime(1990, 1, 1), GenderInterests = GenderInterestEnum.Women },
+                                                   new Profile { Nickname = "CoderKitten", City = "Utrecht", Gender = GenderEnum.Female, Height = 180, Education = EducationEnum.University, Ethnicity = EthnicityEnum.European, Birthdate = new DateTime(1995, 1, 1), GenderInterests = GenderInterestEnum.Men },
+                                                                new Profile { Nickname = "JavaBabe", City = "Schagen", Gender = GenderEnum.Female, Height = 180, Education = EducationEnum.University, Ethnicity = EthnicityEnum.European, Birthdate = new DateTime(1995, 1, 1), GenderInterests = GenderInterestEnum.Men },
+                                                   new Profile { Nickname = "Andrea Peters", City = "Alkmaar", Gender = GenderEnum.Female, Height = 180, Education = EducationEnum.Highschool, Ethnicity = EthnicityEnum.European, Birthdate = new DateTime(1990, 1, 1), GenderInterests = GenderInterestEnum.Men }
+            );
+
+        }
+    }
+}
